c49d1c530ee3d42a6696cc0c4cee5194cb7210c3
zhanghonghuan <zhanghuan1991@163.com>
2020-08-06 16:15:27 +0800

    有情绪化的石头剪刀布小游戏

--- 2/lib.js
+++ 2/lib.js
@@ -1,33 +1,33 @@
-exports.add = function (a, b) {
-    return a - b;
-}
-
-// module.exports = function (playerActive) {
-//     var random = Math.random() * 3;
-
-//     //电脑随机行为
-//     var computerActive = '';
-//     if (random < 1) {
-//         computerActive = '石头';
-//     } else if (random > 2) {
-//         computerActive = '剪刀';
-//     } else {
-//         computerActive = '布';
-//     }
-
-//     //判断逻辑
-//     if (
-//         computerActive === '石头' && playerActive === '布' ||
-//         computerActive === '剪刀' && playerActive === '石头' ||
-//         computerActive === '布' && playerActive === '剪刀'
-//     ) {
-//         console.log('你赢了');
-//         return -1;
-//     } else if (computerActive === playerActive) {
-//         console.log('平局');
-//         return 0;
-//     } else {
-//         console.log('你输了');
-//         return 1;
-//     }
+// exports.add = function (a, b) {
+//     return a - b;
 // }
+
+module.exports = function (playerActive) {
+    var random = Math.random() * 3;
+
+    //电脑随机行为
+    var computerActive = '';
+    if (random < 1) {
+        computerActive = '石头';
+    } else if (random > 2) {
+        computerActive = '剪刀';
+    } else {
+        computerActive = '布';
+    }
+
+    //判断逻辑
+    if (
+        computerActive === '石头' && playerActive === '布' ||
+        computerActive === '剪刀' && playerActive === '石头' ||
+        computerActive === '布' && playerActive === '剪刀'
+    ) {
+        console.log('你赢了');
+        return -1;
+    } else if (computerActive === playerActive) {
+        console.log('平局');
+        return 0;
+    } else {
+        console.log('你输了');
+        return 1;
+    }
+}
