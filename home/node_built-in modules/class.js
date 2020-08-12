b81c07885c2e18e6ae9a2283c677f03fcf75dafa
zhanghonghuan <zhanghuan1991@163.com>
2020-08-12 18:29:09 +0800

    Es6中class的用法及扩展方法

--- home/node_built-in modules/class.js
+++ home/node_built-in modules/class.js
@@ -45,3 +45,73 @@ class Foo {
 console.log(typeof Foo);
 const result = (Foo === Foo.prototype.constructor);
 console.log(result);
+
+/* 
+ ** 上面代码表明，类的数据类型就是函数，类本身就指向构造函数。
+ ** 使用的时候，也是直接对类使用new命令，跟构造函数的用法完全一致。
+ */
+
+class Bar {
+    doStuff() {
+        console.log('stuff');
+    }
+}
+let b = new Bar();
+b.doStuff(); // "stuff"
+
+/* 
+ ** 构造函数的prototype属性，在 ES6 的“类”上面继续存在。
+ ** 事实上，类的所有方法都定义在类的prototype属性上面。
+ */
+
+class Point {
+    constructor() {
+        // ...
+    }
+
+    toString() {
+        // ...
+    }
+
+    toValue() {
+        // ...
+    }
+}
+
+// 等同于
+
+Point.prototype = {
+    constructor() {},
+    toString() {},
+    toValue() {},
+};
+
+
+// 在类的实例上面调用方法，其实就是调用原型上的方法。
+
+class B {}
+let b = new B();
+
+b.constructor === B.prototype.constructor // true
+
+/* 
+ ** 上面代码中，b是B类的实例，它的constructor方法就是B类原型的constructor方法。
+ ** 由于类的方法都定义在prototype对象上面，所以类的新方法可以添加在prototype对象上面。
+ */
+
+class Point {
+    constructor() {
+        //...
+    }
+}
+/* 
+ ** Object.assign方法可以很方便地一次向类添加多个方法。
+ */
+Object.assign(Point.prototype, {
+    toString() {
+        //...
+    },
+    toValue() {
+        //...
+    }
+})
